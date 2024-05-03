34533d2f19ee08024685b982aa81ead31a74b6de
FranciscoMainero <mainerofrancisco@gmail.com>
2024-05-03 05:38:18 -0300

    Fixed crash, fixed memory leaks, talk ended. TODO: proceed to inspect other properties of coroutines.

--- main.cpp
+++ main.cpp
@@ -65,7 +65,7 @@ concept is_awaiter = requires { [](T t) -> is_awaiter_test { co_await t; }; };
 
 struct task {
 	struct promise_type {
-		std::coroutine_handle<> continuation;
+		std::coroutine_handle<> continuation{std::noop_coroutine()}; // Initialization fix crash.
 
 		std::exception_ptr error{};
 
@@ -77,11 +77,13 @@ struct task {
 		// On final_suspend creates and awaiter which make stopped promise resume (to resume suspended task g())
 		struct final_awaiter : std::suspend_always {
 			promise_type *promise;
-			void await_suspend(auto) noexcept { promise->continuation.resume(); }
+
+			// By returning the coroutine handle it gets resumed. Synchronous transfer (search).
+			std::coroutine_handle<> await_suspend(auto) noexcept { return promise->continuation; }
 		};
 		final_awaiter final_suspend() noexcept { return {{}, this}; };
 
-		task get_return_object() { return {this}; }
+		task get_return_object() { return task{unique_promise(this)}; }
 		void unhandled_exception() { error = std::current_exception(); }
 
 		void return_void() {}
@@ -101,7 +103,11 @@ struct task {
 		}
 	};
 
-	promise_type *promise;
+	// deleter: Function wich  givena  promise, gets the handles and destroys it.
+	using deleter =
+		decltype([](promise_type *promise) { std::coroutine_handle<promise_type>::from_promise(*promise).destroy(); });
+	using unique_promise = std::unique_ptr<promise_type, deleter>;
+	unique_promise promise; // Avoid memory leaks.
 
 	// Give a function to start the coroutine
 	void start() {
@@ -122,7 +128,7 @@ struct task {
 		void await_resume() {}
 	};
 
-	nested_awaiter operator co_await() { return nested_awaiter{promise}; }
+	nested_awaiter operator co_await() { return nested_awaiter{promise.get()}; }
 };
 
 int to_be_made_async() { return 17; }
@@ -140,6 +146,10 @@ task f(io &c) {
 
 	std::cout << "f end \n";
 }
+
+// TODO: Next step, from talk notes, co_return values from a task. return_void/return_value. Check out github page for
+// the talk: https://github.com/dietmarkuehl/co_await-all-the-things/tree/main . Inspect libraries wich facilitate
+// coroutines.
 
 int main() {
 	std::cout << std::unitbuf << std::boolalpha;
